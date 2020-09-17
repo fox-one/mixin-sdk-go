fd299daf2994b7ac7be40d5be2e742cb31b9891a
yiplee <guoyinl@gmail.com>
2020-09-17 11:04:26 +0800

    custom ed25519 base64 encoder

--- ed25519.go
+++ ed25519.go
@@ -10,4 +10,19 @@ func GenerateEd25519Key() ed25519.PrivateKey {
 	return private
 }
 
-var ed25519Encoding = base64.RawURLEncoding
+var ed25519Encoding = &ed25519Encoder{}
+
+type ed25519Encoder struct{}
+
+func (enc *ed25519Encoder) EncodeToString(b []byte) string {
+	return base64.RawURLEncoding.EncodeToString(b)
+}
+
+func (enc *ed25519Encoder) DecodeString(s string) ([]byte, error) {
+	b, err := base64.RawURLEncoding.DecodeString(s)
+	if err != nil {
+		b, err = base64.StdEncoding.DecodeString(s)
+	}
+
+	return b, err
+}
