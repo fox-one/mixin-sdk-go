41559fe80274c25f3647820ea00de3e076d66f27
xuhui <xuhuiandfish@gmail.com>
2021-05-13 19:55:53 +0800

    update unfavorite app function

--- app.go
+++ app.go
@@ -84,11 +84,10 @@ func (c *Client) FavoriteApp(ctx context.Context, appID string) (*FavoriteApp, e
 	return &app, nil
 }
 
-func (c *Client) UnFavoriteApp(ctx context.Context, appID string) error {
+func (c *Client) UnfavoriteApp(ctx context.Context, appID string) error {
 	uri := fmt.Sprintf("/apps/%s/unfavorite", appID)
 
-	var result interface{}
-	if err := c.Post(ctx, uri, nil, &result); err != nil {
+	if err := c.Post(ctx, uri, nil, nil); err != nil {
 		return err
 	}
 
