49ecd7caeb687bf0f33797996d422b870c3552a0
yiplee <guoyinl@gmail.com>
2022-01-01 15:52:51 +0800

    rename ReadExchangeRates to ReadFiats

--- fiats.go
+++ fiats.go
@@ -6,15 +6,37 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-type ExchangeRate struct {
+// Fiat is a struct for fiat currencies
+type Fiat struct {
 	Code string          `json:"code,omitempty"`
 	Rate decimal.Decimal `json:"rate,omitempty"`
 }
 
-func (c *Client) ReadExchangeRates(ctx context.Context) ([]ExchangeRate, error) {
-	var rates []ExchangeRate
+// ReadFiats returns the exchange rates between two currencies
+func (c *Client) ReadFiats(ctx context.Context) ([]Fiat, error) {
+	var rates []Fiat
 	if err := c.Get(ctx, "/fiats", nil, &rates); err != nil {
 		return nil, err
+	}
+
+	return rates, nil
+}
+
+// ExchangeRate represent the exchange rate between two currencies
+// deprecated: use Fiat instead
+type ExchangeRate Fiat
+
+// ReadExchangeRates returns the exchange rates between two currencies
+// deprecated: use ReadFiats instead
+func (c *Client) ReadExchangeRates(ctx context.Context) ([]ExchangeRate, error) {
+	fiats, err := c.ReadFiats(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var rates []ExchangeRate
+	for _, fiat := range fiats {
+		rates = append(rates, ExchangeRate(fiat))
 	}
 
 	return rates, nil
