dd91709c874662dff4c3b1a9acc580cf950e8fad
touge <touge1027@gmail.com>
2023-03-16 13:48:16 +0800

    Support get code information

---
+++ code.go
@@ -0,0 +1,133 @@
+package mixin
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
+type CodeType string
+
+const (
+	TypeUser         CodeType = "user"
+	TypeConversation CodeType = "conversation"
+	TypePayment      CodeType = "payment"
+	TypeMultisig     CodeType = "multisig_request"
+	TypeCollectible  CodeType = "non_fungible_request"
+)
+
+type Code struct {
+	Type CodeType `json:"type"`
+	Data interface{}
+}
+
+func (c Code) User() *User {
+	user, ok := c.Data.(User)
+	if !ok {
+		return nil
+	}
+	return &user
+}
+
+func (c Code) Conversation() *Conversation {
+	conversation, ok := c.Data.(Conversation)
+	if !ok {
+		return nil
+	}
+	return &conversation
+}
+
+func (c Code) Payment() *Payment {
+	payment, ok := c.Data.(Payment)
+	if !ok {
+		return nil
+	}
+	return &payment
+}
+
+func (c Code) Multisig() *MultisigRequest {
+	multisig, ok := c.Data.(MultisigRequest)
+	if !ok {
+		return nil
+	}
+	return &multisig
+}
+
+func (c Code) Collectible() *CollectibleRequest {
+	collectible, ok := c.Data.(CollectibleRequest)
+	if !ok {
+		return nil
+	}
+	return &collectible
+}
+
+func (c *Client) GetCode(ctx context.Context, codeString string) (*Code, error) {
+	uri := fmt.Sprintf("/codes/%s", codeString)
+	r, err := c.Request(ctx).SetQueryParams(nil).Get(uri)
+	if err != nil {
+		if requestID := extractRequestID(r); requestID != "" {
+			return nil, WrapErrWithRequestID(err, requestID)
+		}
+
+		return nil, err
+	}
+
+	if requestID := extractRequestID(r); requestID != "" {
+		defer bindRequestID(&err, requestID)
+	}
+
+	data, err := DecodeResponse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return decodeCode(data)
+}
+
+func decodeCode(data []byte) (*Code, error) {
+	var code Code
+	err := json.Unmarshal(data, &code)
+	if err != nil {
+		return nil, err
+	}
+
+	switch code.Type {
+	case TypeUser:
+		var user User
+		err := json.Unmarshal(data, &user)
+		if err != nil {
+			return nil, err
+		}
+		code.Data = user
+	case TypeConversation:
+		var conversation Conversation
+		err := json.Unmarshal(data, &conversation)
+		if err != nil {
+			return nil, err
+		}
+		code.Data = conversation
+	case TypePayment:
+		var payment Payment
+		err := json.Unmarshal(data, &payment)
+		if err != nil {
+			return nil, err
+		}
+		code.Data = payment
+	case TypeMultisig:
+		var multisigRequest MultisigRequest
+		err := json.Unmarshal(data, &multisigRequest)
+		if err != nil {
+			return nil, err
+		}
+		code.Data = multisigRequest
+	case TypeCollectible:
+		var collectibleRequest CollectibleRequest
+		err := json.Unmarshal(data, &collectibleRequest)
+		if err != nil {
+			return nil, err
+		}
+		code.Data = collectibleRequest
+	}
+
+	return &code, nil
+}
---
+++ code_test.go
@@ -0,0 +1,34 @@
+package mixin
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetCode(t *testing.T) {
+	ctx := context.Background()
+	store := newKeystoreFromEnv(t)
+
+	c, err := NewFromKeystore(store)
+	require.Nil(t, err, "init client")
+
+	code, err := c.GetCode(ctx, "c76310d8-c563-499e-9866-c61ae2cbee11")
+	require.Nil(t, err, "get code")
+	require.True(t, code.Type == TypePayment)
+	payment := code.Payment()
+	require.True(t, payment.Amount == "1")
+
+	code, err = c.GetCode(ctx, "d4b174c2-2691-4289-b4a0-2d0f9ec43618")
+	require.Nil(t, err, "get code")
+	require.True(t, code.Type == TypePayment)
+	payment = code.Payment()
+	require.True(t, payment.Amount == "1")
+
+	code, err = c.GetCode(ctx, "e0a53283-da39-438c-ba94-8d77071e9860")
+	require.Nil(t, err, "get code")
+	require.True(t, code.Type == TypeConversation)
+	conversation := code.Conversation()
+	require.True(t, conversation.Category == "GROUP")
+}
