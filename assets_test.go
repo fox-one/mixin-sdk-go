015f83c63602e96a9ecf98f5eec44a5e272c9d88
touge <touge1027@gmail.com>
2023-03-20 16:26:24 +0800

    Asset add depositEntries

---
+++ assets_test.go
@@ -0,0 +1,20 @@
+package mixin
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestReadAsset(t *testing.T) {
+	ctx := context.Background()
+	store := newKeystoreFromEnv(t)
+
+	c, err := NewFromKeystore(store)
+	require.Nil(t, err, "init client")
+
+	asset, err := c.ReadAsset(ctx, "c6d0c728-2624-429b-8e0d-d9d19b6592fa")
+	require.Nil(t, err, "read asset")
+	require.True(t, asset.DepositEntries != nil && len(asset.DepositEntries) > 0, "bitcoin missing segwit address")
+}
