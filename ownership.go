4889ad4b9434c7dcc82fe6699492b231b05e6822
charlie <ys.wu35@gmail.com>
2023-04-08 23:44:48 +0900

    remove debug output

--- ownership.go
+++ ownership.go
@@ -2,7 +2,6 @@ package mixin
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 )
 
@@ -23,15 +22,9 @@ func (c *Client) TransferOwnership(ctx context.Context, newOwner, pin string) er
 		),
 	}
 
-	var resp json.RawMessage
 	uri := fmt.Sprintf("/apps/%s/transfer", c.ClientID)
-	if err := c.Post(ctx, uri, body, &resp); err != nil {
+	if err := c.Post(ctx, uri, body, nil); err != nil {
 		return err
-	}
-
-	{
-		bts, _ := json.MarshalIndent(resp, "", "    ")
-		fmt.Println(string(bts))
 	}
 
 	return nil
