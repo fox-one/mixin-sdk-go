b82c78a73e2b09aa31b700c8aeb73459aa52ee11
charlie <ys.wu35@gmail.com>
2023-08-08 10:33:56 +0900

    wallet example test tip pin transfer

--- _examples/wallet/main.go
+++ _examples/wallet/main.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"crypto/ed25519"
 	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"flag"
 	"log"
 	"os"
+	"time"
 
 	"github.com/fox-one/mixin-sdk-go"
 	"github.com/shopspring/decimal"
@@ -38,8 +40,7 @@ func main() {
 		log.Panicln(err)
 	}
 
-	me, err := client.UserMe(ctx)
-	if err != nil {
+	if _, err := client.UserMe(ctx); err != nil {
 		log.Printf("UserMe: %v", err)
 		return
 	}
@@ -49,74 +50,142 @@ func main() {
 		return
 	}
 
-	if app := me.App; app != nil {
-		input := &mixin.TransferInput{
-			AssetID:    "965e5c6e-434c-3fa9-b780-c50f43cd955c", // CNB
-			OpponentID: app.CreatorID,
-			Amount:     decimal.NewFromInt(100),
-			// THIS IS AN EXAMPLE.
-			// NEVER USE A RANDOM TRACE ID IN YOU REAL PROJECT.
-			TraceID:    mixin.RandomTraceID(),
-			Memo:       "test",
-		}
+	{
+		createAndTestUser(ctx, client, mixin.NewKey(rand.Reader).String())
+	}
+	{
+		_, privateKey, _ := ed25519.GenerateKey(rand.Reader)
+		createAndTestUser(ctx, client, hex.EncodeToString(privateKey))
+	}
 
-		snapshot, err := client.Transfer(ctx, input, *pin)
+	{
+		// create sub wallet
+		// privateKey, _ := rsa.GenerateKey(rand.Reader, 1024)
+		_, privateKey, _ := ed25519.GenerateKey(rand.Reader)
+		sub, subStore, err := client.CreateUser(ctx, privateKey, "sub user")
 		if err != nil {
-			switch {
-			case mixin.IsErrorCodes(err, mixin.InsufficientBalance):
-				log.Println("insufficient balance")
-			default:
-				log.Printf("transfer: %v", err)
-			}
-
-			return
+			log.Panicf("CreateUser: %v", err)
 		}
 
-		log.Println("transfer done", snapshot.SnapshotID, snapshot.Memo)
+		log.Println("create sub user", sub.UserID)
 
-		transfer, err := client.ReadTransfer(ctx, snapshot.TraceID)
-		if err != nil {
-			log.Printf("ReadTransfer: %v", err)
-			return
+		// set pin
+		newPin := mixin.RandomPin()
+		subClient, _ := mixin.NewFromKeystore(subStore)
+		log.Println("try ModifyPin", newPin)
+		if err := subClient.ModifyPin(ctx, "", newPin); err != nil {
+			log.Panicf("ModifyPin (%s) failed: %v", newPin, err)
 		}
 
-		if transfer.SnapshotID != snapshot.SnapshotID {
-			log.Printf("expect %v but got %v", snapshot.SnapshotID, transfer.SnapshotID)
+		tipPin := mixin.NewKey(rand.Reader)
+		log.Println("try ModifyPin", tipPin, tipPin.Public())
+		if err := subClient.ModifyPin(ctx, newPin, tipPin.Public().String()); err != nil {
+			log.Panicf("ModifyPin (%s) failed: %v", tipPin, err)
 		}
 
-		if _, err := client.ReadSnapshot(ctx, snapshot.SnapshotID); err != nil {
-			log.Printf("read snapshot: %v", err)
-			return
+		if err := subClient.VerifyPin(ctx, tipPin.String()); err != nil {
+			log.Panicf("sub user VerifyPin: %v", err)
 		}
 	}
 
+	{
+		// create sub wallet
+		// privateKey, _ := rsa.GenerateKey(rand.Reader, 1024)
+		_, privateKey, _ := ed25519.GenerateKey(rand.Reader)
+		sub, subStore, err := client.CreateUser(ctx, privateKey, "sub user")
+		if err != nil {
+			log.Panicf("CreateUser: %v", err)
+		}
+
+		log.Println("create sub user", sub.UserID)
+
+		// set pin
+		subClient, _ := mixin.NewFromKeystore(subStore)
+		anotherTipPin := hex.EncodeToString(mixin.GenerateEd25519Key())
+		log.Println("try ModifyPin", anotherTipPin[:64], anotherTipPin[64:])
+		if err := subClient.ModifyPin(ctx, "", anotherTipPin[64:]); err != nil {
+			log.Panicf("ModifyPin (%s) failed: %v", anotherTipPin, err)
+		}
+
+		if err := subClient.VerifyPin(ctx, anotherTipPin); err != nil {
+			log.Panicf("sub user VerifyPin: %v", err)
+		}
+	}
+}
+
+func createAndTestUser(ctx context.Context, dapp *mixin.Client, userPin string) {
 	// create sub wallet
 	// privateKey, _ := rsa.GenerateKey(rand.Reader, 1024)
 	_, privateKey, _ := ed25519.GenerateKey(rand.Reader)
-	sub, subStore, err := client.CreateUser(ctx, privateKey, "sub user")
+	sub, subStore, err := dapp.CreateUser(ctx, privateKey, "sub user")
 	if err != nil {
-		log.Printf("CreateUser: %v", err)
-		return
+		log.Panicf("CreateUser: %v", err)
 	}
-
 	log.Println("create sub user", sub.UserID)
+
+	testTransfer(ctx, dapp, *pin, sub.UserID)
 
 	// set pin
 	newPin := mixin.RandomPin()
 	subClient, _ := mixin.NewFromKeystore(subStore)
+	log.Println("try ModifyPin", newPin)
 	if err := subClient.ModifyPin(ctx, "", newPin); err != nil {
-		log.Printf("ModifyPin: %v", err)
+		log.Panicf("ModifyPin (%s) failed: %v", newPin, err)
+	}
+
+	tipPin, err := mixin.KeyFromString(userPin)
+	if err != nil {
+		log.Panicf("KeyFromString(%s) failed: %v", userPin, err)
+	}
+	log.Println("try ModifyPin", tipPin, tipPin.Public())
+	if err := subClient.ModifyPin(ctx, newPin, tipPin.Public().String()); err != nil {
+		log.Panicf("ModifyPin (%s) failed: %v", tipPin, err)
+	}
+
+	if err := subClient.VerifyPin(ctx, userPin); err != nil {
+		log.Panicf("sub user VerifyPin: %v", err)
+	}
+
+	testTransfer(ctx, subClient, userPin, dapp.ClientID)
+}
+
+func testTransfer(ctx context.Context, dapp *mixin.Client, pin, opponent string) {
+	input := &mixin.TransferInput{
+		AssetID:    "965e5c6e-434c-3fa9-b780-c50f43cd955c", // CNB
+		OpponentID: opponent,
+		Amount:     decimal.NewFromInt(100),
+		// THIS IS AN EXAMPLE.
+		// NEVER USE A RANDOM TRACE ID IN YOU REAL PROJECT.
+		TraceID: mixin.RandomTraceID(),
+		Memo:    "test",
+	}
+
+	snapshot, err := dapp.Transfer(ctx, input, pin)
+	if err != nil {
+		switch {
+		case mixin.IsErrorCodes(err, mixin.InsufficientBalance):
+			log.Println("insufficient balance")
+		default:
+			log.Printf("transfer: %v", err)
+		}
+
 		return
 	}
 
-	anotherPin := mixin.RandomPin()
-	if err := subClient.ModifyPin(ctx, newPin, anotherPin); err != nil {
-		log.Printf("ModifyPin with pin: %v", err)
-		return
+	log.Println("transfer done", snapshot.SnapshotID, snapshot.Memo)
+	log.Println("sleep 5 seconds")
+	time.Sleep(5 * time.Second)
+
+	transfer, err := dapp.ReadTransfer(ctx, snapshot.TraceID)
+	if err != nil {
+		log.Panicf("ReadTransfer: %v", err)
 	}
 
-	if err := subClient.VerifyPin(ctx, anotherPin); err != nil {
-		log.Printf("sub user VerifyPin: %v", err)
-		return
+	if transfer.SnapshotID != snapshot.SnapshotID {
+		log.Panicf("expect %v but got %v", snapshot.SnapshotID, transfer.SnapshotID)
+	}
+
+	if _, err := dapp.ReadSnapshot(ctx, snapshot.SnapshotID); err != nil {
+		log.Panicf("read snapshot: %v", err)
 	}
 }
