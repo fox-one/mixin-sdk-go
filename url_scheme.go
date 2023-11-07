f7a2b2235560737f1837c35a196f2688abedef0e
Halimao <1065621723@qq.com>
2023-11-07 10:56:35 +0800

    feat: support more scheme

--- url_scheme.go
+++ url_scheme.go
@@ -1,10 +1,22 @@
 package mixin
 
 import (
+	"encoding/base64"
 	"net/url"
 )
 
 const Scheme = "mixin"
+
+type SendSchemeCategory = string
+
+const (
+	SendSchemeCategoryText    SendSchemeCategory = "text"
+	SendSchemeCategoryImage   SendSchemeCategory = "image"
+	SendSchemeCategoryContact SendSchemeCategory = "contact"
+	SendSchemeCategoryAppCard SendSchemeCategory = "app_card"
+	SendSchemeCategoryLive    SendSchemeCategory = "live"
+	SendSchemeCategoryPost    SendSchemeCategory = "post"
+)
 
 var URL urlScheme
 
@@ -72,6 +84,86 @@ func (urlScheme) Snapshots(snapshotID, traceID string) string {
 		query.Set("trace", traceID)
 		u.RawQuery = query.Encode()
 	}
+
+	return u.String()
+}
+
+// Conversations scheme of a conversation
+//
+//	userID optional, for user conversation only, if there's not conversation with the user, messenger will create the conversation first
+//
+// https://developers.mixin.one/docs/schema#open-an-conversation
+func (urlScheme) Conversations(conversationID, userID string) string {
+	u := url.URL{
+		Scheme: Scheme,
+		Host:   "conversations",
+	}
+
+	if conversationID != "" {
+		u.Path = conversationID
+	}
+
+	if userID != "" {
+		query := url.Values{}
+		query.Set("user", userID)
+		u.RawQuery = query.Encode()
+	}
+
+	return u.String()
+}
+
+// Apps scheme of an app
+//
+//	appID required, userID of an app
+//	action optional, action about this scheme, default is "open"
+//	params optional, parameters of any name or type can be passed when opening the bot homepage to facilitate the development of features like invitation codes, visitor tracking, etc
+//
+// https://developers.mixin.one/docs/schema#popups-bot-profile
+func (urlScheme) Apps(appID, action string, params map[string]string) string {
+	u := url.URL{
+		Scheme: Scheme,
+		Host:   "apps",
+	}
+
+	if appID != "" {
+		u.Path = appID
+	}
+
+	query := url.Values{}
+	if action != "" {
+		query.Set("action", action)
+	} else {
+		query.Set("action", "open")
+	}
+	for k, v := range params {
+		query.Set(k, v)
+	}
+	u.RawQuery = query.Encode()
+
+	return u.String()
+}
+
+// Send scheme of a share
+//
+//	category required, category of shared content
+//	data required, shared content
+//	conversationID optional, If you specify conversation and it is the conversation of the user's current session, the confirmation box shown above will appear, the message will be sent after the user clicks the confirmation; if the conversation is not specified or is not the conversation of the current session, an interface where the user chooses which session to share with will show up.
+//
+// https://developers.mixin.one/docs/schema#sharing
+func (urlScheme) Send(category SendSchemeCategory, data []byte, conversationID string) string {
+	u := url.URL{
+		Scheme: Scheme,
+		Host:   "send",
+	}
+	query := url.Values{}
+	query.Set("category", category)
+	if len(data) > 0 {
+		query.Set("data", url.QueryEscape(base64.StdEncoding.EncodeToString(data)))
+	}
+	if conversationID != "" {
+		query.Set("conversation", conversationID)
+	}
+	u.RawQuery = query.Encode()
 
 	return u.String()
 }
--- url_scheme_test.go
+++ url_scheme_test.go
@@ -11,3 +11,27 @@ func Test_urlScheme_Transfer(t *testing.T) {
 	url := URL.Transfer(userID)
 	assert.Equal(t, "mixin://transfer/"+userID, url)
 }
+
+func Test_urlScheme_Apps(t *testing.T) {
+	t.Run("default action", func(t *testing.T) {
+		appID := newUUID()
+		action := ""
+		url := URL.Apps(appID, action, nil)
+		assert.Equal(t, "mixin://apps/"+appID+"?action=open", url)
+	})
+	t.Run("specify action", func(t *testing.T) {
+		appID := newUUID()
+		action := "close"
+		url := URL.Apps(appID, action, nil)
+		assert.Equal(t, "mixin://apps/"+appID+"?action="+action, url)
+	})
+	t.Run("specify params", func(t *testing.T) {
+		appID := newUUID()
+		action := ""
+		params := map[string]string{"k1": "v1", "k2": "v2"}
+		url := URL.Apps(appID, action, params)
+		assert.Contains(t, url, "mixin://apps/"+appID+"?action=open")
+		assert.Contains(t, url, "k1=v1")
+		assert.Contains(t, url, "k2=v2")
+	})
+}
