1e541405e197aaf0e12b8ad8f216af8421359824
charlie <ys.wu35@gmail.com>
2023-11-20 14:46:44 +0900

    typo; add mix address;

--- safe_ghost_keys.go
+++ safe_ghost_keys.go
@@ -2,6 +2,7 @@ package mixin
 
 import (
 	"context"
+	"crypto/rand"
 
 	"github.com/shopspring/decimal"
 )
@@ -36,4 +37,31 @@ func (c *Client) SafeCreateGhostKeys(ctx context.Context, inputs []*GhostKeyInpu
 	}
 
 	return resp, nil
+}
+
+func (c *Client) SafeCreateMixAddressGhostKeys(ctx context.Context, trace string, ma *MixAddress, outputIndex uint) (*GhostKeys, error) {
+	if len(ma.xinMembers) > 0 {
+		r := NewKey(rand.Reader)
+		gkr := &GhostKeys{
+			Mask: r.Public(),
+			Keys: make([]Key, len(ma.xinMembers)),
+		}
+		for i, a := range ma.xinMembers {
+			k := DeriveGhostPublicKey(&r, &a.PublicViewKey, &a.PublicSpendKey, uint64(outputIndex))
+			gkr.Keys[i] = *k
+		}
+		return gkr, nil
+	}
+
+	gks, err := c.SafeCreateGhostKeys(ctx, []*GhostKeyInput{
+		{
+			Receivers: ma.Members(),
+			Index:     int(ma.Threshold),
+			Hint:      trace,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return gks[0], nil
 }
